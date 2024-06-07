250cd82086e2ff8f2bc19f515eefcdd08bc9a19c
sowmya-ram <79638562+sowmya-ram@users.noreply.github.com>
2024-06-07 15:51:23 +0530

    Update Grid.js

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -8,7 +8,9 @@ const Grid = ({ grid, robot }) => {
       {grid.map((row, rowIndex) => (
         <div key={rowIndex} className="row">
           {row.map((cell, cellIndex) => (
+            // x and y will be the coordinates and rowIndex and cellIndex
             <div key={cellIndex} className={`cell ${robot.x === cellIndex && robot.y === rowIndex ? 'robot' : ''}`}>
+              {/* If the rowIndex and cellIndex the currect cell then place the robot */}
               {robot.x === cellIndex && robot.y === rowIndex && <div className={`robot-icon ${robot.direction}`}><SmartToyIcon/></div>}
             </div>
           ))}
